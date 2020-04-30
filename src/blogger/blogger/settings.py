9760567a2cd4e6bddcd663fdcf7729dee8adc8d0
oussama tamsaft <oussamatamsaft@gmail.com>
2020-04-30 23:47:07 +0200

    include static files

--- src/blogger/blogger/settings.py
+++ src/blogger/blogger/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#static for all apps
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+#     'C:/Users/tamsaft/Desktop/Django/blog/blog/src/blogger/static'
+# ]
