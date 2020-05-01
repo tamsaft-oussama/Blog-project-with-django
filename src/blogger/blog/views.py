6c8c6880cc628125d00ff5605fe0cf178edfa898
oussama tamsaft <oussamatamsaft@gmail.com>
2020-05-01 17:23:30 +0200

    i create blog template

--- src/blogger/blog/views.py
+++ src/blogger/blog/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
 
+# fake posts for test
+posts = [
+    {
+        'title':'Learn Django 3',
+        'content':'Leran how to create app web with django 3 '
+    },
+        {
+        'title':'Learn Laravel 7',
+        'content':'Leran how to create app web with Laravel 7 '
+    },
+        {
+        'title':'Learn Angular 8',
+        'content':'Leran how to create app web with Angular 8 '
+    },
+        {
+        'title':'Learn React',
+        'content':'Leran how to create app web with React '
+    },
+]
 # Create your views here.
 def home(request):
-    return render(request,'blog/index.html',{});
+    context = {
+        'title' : 'Home',
+        'posts' : posts,
+    }
+    return render(request,'blog/index.html',context);
